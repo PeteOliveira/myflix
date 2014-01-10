866b13d87cd6f9f7d1a88a972acdb38565494669
ghar <pete@elusivestars.com>
2014-01-09 18:40:44 -0700

    configured mail catcher and bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,4 +18,25 @@ Myflix::Application.configure do
   config.assets.debug = true
 
   config.eager_load = false
+
+  # mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :host => "localhost", :port => 1025 }
+
+  # Bullet
+  config.after_initialize do
+    Bullet.enable = true
+    #Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    #Bullet.growl = true
+    #Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
+    #                :password => 'bullets_password_for_jabber',
+    #                :receiver => 'your_account@jabber.org',
+    #                :show_online_status => true }
+    Bullet.rails_logger = true
+    #Bullet.airbrake = true
+    Bullet.add_footer = true
+  end
+
 end
