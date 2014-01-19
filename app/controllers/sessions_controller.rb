3280b8a1a7c59994fa994f9dfab84e3b95e9e0e7
ghar <pete@elusivestars.com>
2014-01-18 21:33:05 -0700

    Implement authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,23 @@
 class SessionsController < ApplicationController
   def new
-
+    redirect_to home_path if current_user
   end
+
+  def create
+    user = User.where(email: params[:email]).first # returns array unless we specify first
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to home_path, notice: 'You are signed in'
+    else
+      flash[:error] = 'Invalid email and or password'
+      redirect_to sign_in_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Good bye"
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,25 @@
 Myflix::Application.routes.draw do
 
   root to: 'pages#front'
-  get '/register', to: 'users#new'
-  get '/sign_in', to: 'sessions#new'
   get '/home', to: 'videos#index'
 
-  get '/video/:id', to: 'videos#show', as: 'video'
+  get 'register', to: 'users#new'
+  get 'sign_in', to: 'sessions#new'
 
+  get 'sign_out', to: 'sessions#destroy'
+
+  resources :users, only: [:create]
+  resources :sessions, only: [:create]
+
+  get '/video/:id', to: 'videos#show',  as: 'video'
   resources :videos, only: [:show] do
     collection do
       post :search, to: "videos#search"
     end
   end
 
-  get 'ui(/:action)', controller: 'ui'
-
   resources  :categories
 
-  resources :users, only: [:create]
+  get 'ui(/:action)', controller: 'ui'
 
 end
