9c4024b4bd0e4a7c9a9ed1270b1cad2047e90ae0
ghar <pete@elusivestars.com>
2014-01-19 15:24:06 -0700

    Add controller tests for authentication

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -3,14 +3,33 @@ require 'spec_helper'
 describe VideosController do
 
   describe "GET show" do
-    it "sets the @video variable" do
+    it "sets the @video variable for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
       video = Fabricate(:video)
       get :show, id: video.id
       expect(assigns(:video)).to eq(video)
     end
-    it "renders the show template" do
 
+    it "redirects to the sign in page for unauthenticated users" do
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(response).to redirect_to sign_in_path
     end
+  end
+
+  describe "POST search" do
+    it "sets the @results variable for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      movie = Fabricate(:video, title: "Resident Evil")
+      post :search, search_term: 'dent'
+      expect(assigns(:results)).to eq([movie])
+    end
+    it "redirects to the sign in page for unathenticated users" do
+      movie = Fabricate(:video, title: "Resident Evil")
+      post :search, search_term: 'dent'
+      expect(response).to redirect_to sign_in_path
+    end
+
   end
 
 end
