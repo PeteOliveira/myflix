078f3c442fb63eac7117074d11a78071f34c319f
ghar <pete@elusivestars.com>
2014-01-23 22:16:59 -0700

    Push down complexity

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -15,14 +15,14 @@ class QueueItemsController < ApplicationController
     queue_item = QueueItem.find(params[:id])
     queue_item.destroy if current_user.queue_items.include?(queue_item)
     #queue_item.destroy if current_user.id == queue_item.user_id
-    normalize_queue_item_positions
+    current_user.normalize_queue_item_positions
     redirect_to my_queue_path
   end
 
   def update_queue
     begin
       update_queue_items
-      normalize_queue_item_positions
+      current_user.normalize_queue_item_positions
     rescue ActiveRecord::RecordInvalid
       flash[:error] = "Invalid position number"
     end
@@ -30,12 +30,6 @@ class QueueItemsController < ApplicationController
   end
 
   private
-
-  def normalize_queue_item_positions
-    current_user.queue_items.each_with_index do |queue_item, index|
-      queue_item.update_attributes(position: index+1)
-    end
-  end
 
   def update_queue_items
     ActiveRecord::Base.transaction do
